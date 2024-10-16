81a491bbbb2af26962cde44aa1b96c8905c4ebae
Ahmed Esam <ahmedesam415@gmail.com>
2024-10-17 01:06:15 +0300

    Added Optional background And Modefied in css, js

--- Js/master.js
+++ Js/master.js
@@ -1,3 +1,20 @@
+//* Check If There's Local Storage Color Option
+let mainColors = localStorage.getItem("color");
+
+if (mainColors !== null) {
+  document.documentElement.style.setProperty("--main-color", mainColors);
+
+  //* Remove Active Class From All Color List Item
+  document.querySelectorAll(".colors-list li").forEach((element) => {
+    element.classList.remove("active");
+
+    //* Add Active Class On Element With Data-Color === Local Storage Item
+    if (element.dataset.color === mainColors) {
+      element.classList.add("active");
+    }
+  });
+}
+
 //* Toggle Spin Class On Icon
 let toggleOpen = document.querySelector(".setting-box");
 let toggle = document.querySelector(".toggle-setting .fa-gear");
@@ -20,17 +37,66 @@ colorsli.forEach((li) => {
       "--main-color",
       e.target.dataset.color
     );
+    //* Set Color On localStorage
+    localStorage.setItem("color", e.target.dataset.color);
+
+    //* Remove Active Class From All Children
+    e.target.parentElement.querySelectorAll(".active").forEach((element) => {
+      element.classList.remove("active");
+    });
+
+    //* Add Active Class On Self
+    e.target.classList.add("active");
+  });
+});
+
+//* Switch Ramndom BackGround Option
+const randomBackEl = document.querySelectorAll(".random-backgrounds span");
+
+//* Random Background Option
+let backgroundOption = true;
+
+//* Variable To Control The Background Interval
+let backgroundInterval;
+
+//* Loop On All Span
+randomBackEl.forEach((span) => {
+  //* Click On Every span
+  span.addEventListener("click", (e) => {
+    //* Remove Active Class From All Span
+    e.target.parentElement.querySelectorAll(".active").forEach((element) => {
+      element.classList.remove("active");
+    });
+
+    //* Add Active Class On Self
+    e.target.classList.add("active");
+
+    if (e.target.dataset.background === "yes") {
+      backgroundOption = true;
+      RandomizImgs();
+    } else {
+      backgroundOption = false;
+      clearInterval(backgroundInterval);
+    }
   });
 });
 
 //* Select Landing Page Element
 let Landingpage = document.querySelector(".landing-page");
+
 //* Get Array Of Imgs
 let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
-setInterval(() => {
-  //* Get Random Number
-  let randomNumber = Math.floor(Math.random() * imgsArray.length);
 
-  //* Change Background Img Url
-  Landingpage.style.backgroundImage = `url("Imgs/${imgsArray[randomNumber]}")`;
-}, 5000);
+//* Function To Randomiz Imgs
+function RandomizImgs() {
+  if (backgroundOption === true) {
+    backgroundInterval = setInterval(() => {
+      //* Get Random Number
+      let randomNumber = Math.floor(Math.random() * imgsArray.length);
+
+      //* Change Background Img Url
+      Landingpage.style.backgroundImage = `url("Imgs/${imgsArray[randomNumber]}")`;
+    }, 1000);
+  }
+}
+RandomizImgs();
