f34797401bdd1354e0d4d34e53ffc07a30ff4673
Ahmed Esam <ahmedesam415@gmail.com>
2024-10-16 20:50:35 +0300

    Creat setting-box and toggle-setting

--- Js/master.js
+++ Js/master.js
@@ -1,10 +1,32 @@
+//* Toggle Spin Class On Icon
+let toggleOpen = document.querySelector(".setting-box");
+let toggle = document.querySelector(".toggle-setting .fa-gear");
+
+toggle.onclick = function () {
+  //* Toggle Class Fa-spin For Rotation On Self
+  this.classList.toggle("fa-spin");
+  //* Toggle Class Open On Main Setting Box
+  toggleOpen.classList.toggle("open");
+};
+
+//* Switch Colors
+const colorsli = document.querySelectorAll(".colors-list li");
+//* Loop On All List Items
+colorsli.forEach((li) => {
+  //* Click On Every List Item
+  li.addEventListener("click", (e) => {
+    //* Set Color On Root
+    document.documentElement.style.setProperty(
+      "--main-color",
+      e.target.dataset.color
+    );
+  });
+});
+
 //* Select Landing Page Element
-
 let Landingpage = document.querySelector(".landing-page");
-
 //* Get Array Of Imgs
 let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
-
 setInterval(() => {
   //* Get Random Number
   let randomNumber = Math.floor(Math.random() * imgsArray.length);
